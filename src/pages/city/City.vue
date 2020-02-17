333763360d170368249910bdf975d93c48e09145
HouAng <l>
2020-02-17 16:06:39 +0800

    add City Search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
     <div>Hello City</div>
   </div>
 </template>
 
 <script>
 import CityHeader from './compoments/Header'
+import CitySearch from './compoments/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
