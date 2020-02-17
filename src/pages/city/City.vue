bda0c6be90f07de94e570df295047b310bff1378
HouAng <l>
2020-02-17 15:55:16 +0800

    'city-router'

---
+++ src/pages/city/City.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <div>Hello City</div>
+  </div>
+</template>
+
+<script>
+import CityHeader from './compoments/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
