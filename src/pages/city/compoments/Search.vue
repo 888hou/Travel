91225f6d674eeb44dc1c69f0c629fee3a756f540
HouAng <l>
2020-02-21 18:26:13 +0800

    search-finish

--- src/pages/city/compoments/Search.vue
+++ src/pages/city/compoments/Search.vue
@@ -1,28 +1,91 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音"/>
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const resule = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              resule.push(value)
+            }
+          })
+        }
+        this.list = resule
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
     .search
-        height : .72rem
-        background : $bgColor
-        padding : 0 .1rem
+        height: .72rem
+        background: $bgColor
+        padding: 0 .1rem
         .search-input
-          box-sizing :border-box
+          box-sizing: border-box
           width: 100%
-          height :.62rem
-          line-height : .62rem
-          text-align : center
-          border-radius : .06rem
+          height: .62rem
+          line-height: .62rem
+          text-align: center
+          border-radius: .06rem
           color: #666
           padding: 0 .1rem
+      .search-content
+        position: absolute
+        top: 1.58rem
+        left: 0
+        right: 0
+        bottom: 0
+        overflow: hidden
+        background: #eeeeee
+        z-index: 1
+        .search-item
+          line-height: .62rem
+          padding-left: .2rem
+          color: #666
+          background: #ffffff
 </style>
